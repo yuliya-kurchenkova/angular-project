575c840e9cae5ccaf2156d1470672340255c31cc
yuliya-kurchenkova <iulia.kurchenkova@yandex.by>
2024-11-09 00:35:37 +0300

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}",],
+  theme: {
+    extend: { 
+        fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+   },
+  },
+  plugins: [],
+}
+
